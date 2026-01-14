53f7b8d5f9b11705dda7fb426364eaa57a90278b
hippop3c <hippop3c@gmail.com>
2026-01-14 16:14:00 +0800

    Refactor to edge function and remove express

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-import express from "express";
-import fetch from "node-fetch";
+export const config = {
+  runtime: "edge",
+};
 
-const app = express();
+export default async function handler() {
+  const res = await fetch(
+    "https://data.tycg.gov.tw/api/v1/rest/datastore/5ca2bfc7-9ace-4719-88ae-4034b9a5a55c"
+  );
 
-app.get("/api/tycg-youbike", async (req, res) => {
-  try {
-    const upstream = await fetch(
-      "https://data.tycg.gov.tw/api/v1/rest/datastore/5ca2bfc7-9ace-4719-88ae-4034b9a5a55c"
-    );
+  const text = await res.text();
 
-    const text = await upstream.text();
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/json; charset=utf-8");
-    res.send(text);
-  } catch (err) {
-    res.status(500).json({
-      error: true,
-      message: err.toString()
-    });
-  }
-});
-
-export default app;
+  return new Response(text, {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
